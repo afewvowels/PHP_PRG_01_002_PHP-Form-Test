b26219143f9656e97d13dba5e381e4312b7ba701
aeiouandsometimesy <aeiouandsometimesy@me.com>
2019-05-21 18:18:42 -0700

    update

---
+++ includes/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['textinput'])) {
+  post_successful();
+}
+
+function post_successful() {
+  echo 'success!';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html>
+<head>
+  <title>PHP Header and Footer</title>
+  <meta charset='utf-8' />
+  <link rel='stylesheet' href='./css/style.css' type='text/css' />
+</head>
+
+<body>
+  <?php include('./includes/functions.php') ?>
+  <div id='form-wrapper'>
+    <form method='post' action=''>
+      <input name='textinput' type='text' />
+      <input name='submitbutton' type='submit' value='go' />
+    </form>
+  </div>
+</body>
+
+</html>
